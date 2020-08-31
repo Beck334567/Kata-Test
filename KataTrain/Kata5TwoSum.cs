98cd4aa4e16c262a539f4cdf2d2860f698c52ec7
Beck Lin <beck.lin@titansoft.com.sg>
2020-08-31 19:11:23 +0800

    test

--- KataTrain/Kata5TwoSum.cs
+++ KataTrain/Kata5TwoSum.cs
@@ -10,7 +10,7 @@ namespace KataTrain
     {
         public static int[] TwoSum(int[] nums, int target)
         {
-
+            string a = "testgitkraken";
             int[] retn = new int[2];
             Dictionary<int, int> mydic = new Dictionary<int, int>();
             for (int i = 0; i < nums.Length; i++)
