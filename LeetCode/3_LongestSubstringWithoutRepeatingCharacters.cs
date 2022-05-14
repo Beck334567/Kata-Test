5095a4dd449bcdf3ef1093b95938655d152a7250
Beck <sars88967@gmail.com>
2022-05-14 14:56:56 +0800

    Add leetcode unit test

---
+++ LeetCode/3_LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+     public  class LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var length = s.Length;
+            var result = 0;
+            var max = 0;
+            var temp = "";
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+            if (s.Replace(s[0], ' ').Trim() == "")
+            {
+                return 1;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                temp += s[i];
+                result = 1;
+                for (int j = i + 1; j < s.Length; j++)
+                {
+
+                    if (!temp.Contains(s[j].ToString()))
+                    {
+                        result++;
+                        temp += s[j];
+                    }
+                    else break;
+                }
+                temp = "";
+                if (result > max)
+                {
+                    max = result;
+                }
+            }
+            return max;
+        }
+
+        
+    }
+}
---
+++ LeetCodeTest/LeetCodeUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+
+namespace LeetCodeTest
+{
+    [TestFixture()]
+    public class LeetCodeUnitTest
+    {
+        //Leetcode no.3
+        [Test()]
+        [TestCase("abcabcbb", 3)]
+        [TestCase("bbbbb", 1)]
+        [TestCase("pwwkew", 3)]
+        public void LongestSubstringWithoutRepeatingCharacters(string input, int output)
+        {
+            //Arrange
+            var ex = new LongestSubstringWithoutRepeatingCharacters();
+
+            //Act
+            var result = ex.LengthOfLongestSubstring(input);
+
+            //Assert
+            NUnit.Framework.Assert.AreEqual(result, output);
+        }
+
+        //Leetcode no.3
+        [Test()]
+        [TestCase("abcabcbb", 3)]
+        [TestCase("bbbbb", 1)]
+        [TestCase("pwwkew", 3)]
+        public void LongestSubstringWithoutRepeatingCharacters(string input, int output)
+        {
+            //Arrange
+            var ex = new LongestSubstringWithoutRepeatingCharacters();
+
+            //Act
+            var result = ex.LengthOfLongestSubstring(input);
+
+            //Assert
+            NUnit.Framework.Assert.AreEqual(result, output);
+        }
+    }
+}
